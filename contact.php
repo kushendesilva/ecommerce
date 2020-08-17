fbec5ae6e5f316e2eaf2e708bf92cf8ca03f2539
Kushen De Silva <kushenthimira@gmail.com>
2020-08-18 01:24:31 +0530

    Update-1.0

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php include 'inc/header.php'; ?>
+
+ <div class="main">
+    <div class="content">
+    	<div class="section group">
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form>
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>SUBJECT</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="SUBMIT"></span>
+						  </div>
+					    </form>
+				  </div>
+  				</div>
+
+			  </div>    	
+    </div>
+ </div>
+<?php include 'inc/footer.php'; ?>
---
+++ inc/footer.php
@@ -0,0 +1,49 @@
+</div>
+   <div class="footer">
+   	  <div class="wrapper">	
+	     <div class="section group">
+				<div class="col_1_of_4 span_1_of_4">
+						
+					</div>
+				<div class="col_1_of_4 span_1_of_4">
+					
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					
+						<div class="social-icons">
+							
+   	 					</div>
+				</div>
+			</div>
+
+     </div>
+    </div>
+    <script type="text/javascript">
+		$(document).ready(function() {
+
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+    <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+    <link href="css/flexslider.css" rel='stylesheet' type='text/css' />
+	  <script defer src="js/jquery.flexslider.js"></script>
+	  <script type="text/javascript">
+		$(function(){
+		  SyntaxHighlighter.all();
+		});
+		$(window).load(function(){
+		  $('.flexslider').flexslider({
+			animation: "slide",
+			start: function(slider){
+			  $('body').removeClass('loading');
+			}
+		  });
+		});
+	  </script>
+</body>
+</html>
